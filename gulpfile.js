3c60d8fb49da0dc6861686673ad09f303d1b26cb
Dennis Oba <oba.dennis@gmail.com>
2018-12-09 14:30:21 -0500

    Push 4

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+
+gulp.tast('styles', () => {
+	return gulp.src('./dev/styles/**/*.scss')
+	 .pipe(sass().on('error', sass.logError))
+	 .pipe(concat('style.css'))
+	 .pipe(gulp.dest('./public/styles'))
+});
